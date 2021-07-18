60ec18842a7ac40af8ad8aad94927816354ae3e1
Usyk <milausik@ukr.net>
2021-07-19 00:13:21 +0300

    added gallery, delegation

---
+++ colorpiker.js
@@ -0,0 +1,64 @@
+const colors = [
+    { hex: '#f44336', rgb: '244,67,54' },
+    { hex: '#e91e63', rgb: '233,30,99' },
+    { hex: '#9c27b0', rgb: '156,39,176' },
+    { hex: '#673ab7', rgb: '103,58,183' },
+];
+
+const paletteContainer = document.querySelector('.js-palette');
+const cardsMarkup = createColorCardsMarkup(colors);
+
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+paletteContainer.addEventListener('click', onpaletteContainerClick)
+
+function createColorCardsMarkup (colors) {   //создаем палитру
+    return colors
+    .map(({hex, rgb}) => {
+        return `<div class="color-card">
+        <div class="color-swatch"
+        data-hex="${hex}"
+        data-rgb="${rgb}"
+        style="background-color: ${hex}"
+        ></div>
+        <div class="color-meta">
+            <p>HEX: ${hex}</p>
+            <p>RGB: ${rgb}</p>
+        </div>
+        </div>`;
+    })
+    .join('');
+};
+
+
+
+function onpaletteContainerClick(evt) {
+   const isColorSwatchEl = evt.target.classList.contains('color-swatch');
+    if (!isColorSwatchEl) {
+        return;
+    }
+    
+    const swatchEl = evt.target;
+    const parentColorCard = swatchEl.closest('.color-card');
+
+    removeActiveCardClass(); // просто вызов
+    addActiveCardClass(parentColorCard);
+    setBodyBgColor(swatchEl.dataset.hex);
+}
+
+
+function setBodyBgColor (color) {
+    document.body.style.backgroundColor = color; // зарисовка фона
+};
+
+function removeActiveCardClass() {
+    const currentActiveCard = document.querySelector('.color-card.is-active') //нашли текущую активкарту
+
+    if(currentActiveCard) {
+        currentActiveCard.classList.remove('is-active');
+    }
+}
+
+function addActiveCardClass (card) {
+    card.classList.add('is-active');
+} 
--- main.js
+++ main.js
@@ -1,3 +1,44 @@
 import galleryItems from "./app.js";
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.js-gallery');
+const lightboxRef = document.querySelector('js-lightbox');
+const lightboxOverlayRef = document.querySelector('lightbox__overlay');
+const lightboxImageRef = document.querySelector('lightbox__image');
+const lightboxButtonRef = document.querySelector('lightbox__button');
+const lightboxContentRef = document.querySelector('lightbox__content');
+
+//Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.
+const pictureMarkup = createGallery();
+galleryRef.insertAdjacentHTML('beforeend', pictureMarkup);
+
+function createGallery() {
+    let i = 0;
+    const markup = galleryItems
+    .map(({preview, original, description}) => {
+        return `
+        <li class="js-gallery__item">
+            <a class="js-gallery__link" href="${original}">
+                <img class="gallery__image" 
+                src="${preview}" 
+                alt="${description}" 
+                data-source="${original}"
+                data-picture="${i += 1}"
+                />
+            </a>
+        </li>
+    `;})
+    .join('');
+    return markup;
+};
+
+//Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+
+galleryRef.addEventListener('click', onClick);
+
+function onClick(evt) {
+    if (evt.target.nodeName !== 'js-gallery__item') {
+        return;
+    }
+}
+
+
