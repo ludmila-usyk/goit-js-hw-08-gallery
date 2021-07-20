53c2379f2d91768da41e8de053dddfa141d7e4e6
Usyk <milausik@ukr.net>
2021-07-21 00:15:47 +0300

    modal?

--- main.js
+++ main.js
@@ -35,10 +35,116 @@ function createGallery() {
 
 galleryRef.addEventListener('click', onClick);
 
-function onClick(evt) {
-    if (evt.target.nodeName !== 'js-gallery__item') {
+function onClick(e) {
+    if (e.target.nodeName !== 'js-gallery__item') {
         return;
     }
 }
 
 
+//modal
+
+// galleryRef.addEventListener('click', onOpenModal);
+
+// function onOpenModal(e) {
+//     e.preventDefault();
+//     if (e.target.nodeName !== "IMG") {
+//       return;
+//     }
+//     document.addEventListener("keydown", onCloseModal);
+  
+//     lightboxRef.classList.add("is-open");
+//     lightboxImageRef.src = e.target.dataset.source;
+//     lightboxImageRef.alt = e.target.alt;
+//   }
+  
+//   function onCloseModal(e) {
+//     e.preventDefault();
+//     document.removeEventListener("keydown", onCloseModal);
+//     lightboxRef.classList.remove("is-open");
+//     lightboxImageRef.src = "";
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+function onOpenModal(e) {
+    e.stopPropagation();
+    if(e.target.nodeName !== 'IMG') {
+        return;
+    }
+        lightboxRef.classList.add("is-open");
+        lightboxImageRef.src = e.target.dataset.source;
+        lightboxImageRef.alt = e.target.alt;
+        lightboxOverlayRef.addEventListener("click", closeByOverlay);
+        document.addEventListener("keydown", closeByEscape);
+        lightboxButtonRef.addEventListener('click', onCloseModal);
+        window.addEventListener("keydown", lightboxImageRefScroll);
+        lightboxContentRef.addEventListener("click", lightboxImageRefScroll);          
+};
+
+function onCloseModal(e) {
+    lightboxRef.classList.remove("is-open");
+    lightboxOverlayRef.removeEventListener("click", closeByOverlay);
+    document.removeEventListener("keydown", closeByEscape);
+    lightboxButtonRef.removeEventListener('click', onCloseModal);
+    window.removeEventListener("keydown", lightboxImageRefScroll);
+    lightboxContentRef.removeEventListener("click", lightboxImageRefScroll);      
+};
+
+galleryRef.addEventListener('click', onOpenModal);
+// lightboxButtonRef.addEventListener('click', onCloseModal);
+
+
+function closeByEscape(e) {
+    if (e.code === "Escape") {
+        onCloseModal(e)        
+    }
+};
+
+function closeByOverlay(e) {
+    if (e.currentTarget === e.target) {
+        onCloseModal(e)      
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function closeModalESC(evt) {
+//     if (evt.key !== "Escape") {
+//         return;
+//     }
+//     toggleModal();
+// };
+// function toggleModal() {
+//     closeModalESC.classList.toggle("open-modal");
+// };
+// lightboxRef.addEventListener("click", toggleModal);
+// lightboxButtonRef.addEventListener("click", toggleModal);
+// window.addEventListener("keyup", closeModalESC);
