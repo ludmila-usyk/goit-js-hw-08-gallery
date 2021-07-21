9fcca789217239f3c57c96acde325d60f1aff206
Usyk <milausik@ukr.net>
2021-07-21 23:38:27 +0300

    added lazyload

--- main.js
+++ main.js
@@ -62,13 +62,40 @@ function closeByEscape(e) {
 }
 
 
+//lazyload
+if ('loading' in HTMLImageElement.prototype) {
+    addSrcAttrToLazyImages();
+} else {
+    addLazySizesSript();
+}
 
+const lazyImages = document.querySelectorAll('.preview');
 
+lazyImages.forEach(Image => {
+    Image.addEventListener('load', onImageLoaded, {once: true});
+})
 
+function onImageLoaded(e) {
+    e.target.classList.add('appear');
+}
 
+function addLazySizesSript () {
+    const script = document.createElement('script');
 
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+    script.integrity = 'sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==';
+    script.crossOrigin = 'anonymous';
 
+    document.body.appendChild(script);
+};
 
+function addSrcAttrToLazyImages() {
+    const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+
+    lazyImages.forEach(img => {
+        img.preview = img.dataset.src;
+    });
+};
 
 
 
